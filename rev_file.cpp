957e10cdf2efac5754555423aaea54c6fe622cee
Meghna Krishnan <meghna.krishnan@uoit.net>
2020-01-30 15:59:47 -0500

    Completed Assignment 3

--- rev_file.cpp
+++ rev_file.cpp
@@ -43,27 +43,34 @@ int main(int argc, char *argv[])
 void freadnames(ifstream &f,char *list [])
 {
 	char x[200];
-
 	int i = 0;
-
 	// write a while loop to reads string form the file and put it in x till the end file
-		// inside the loop allocate the dynamic array for list[i]
-		// copy string in x to list[i] array
-		// increment i 
+	// inside the loop allocate the dynamic array for list[i]
+	// copy string in x to list[i] array
+	// increment i 
+
+	while (f >> x) {
+		list[i] = new char[strlen(x) + 1];
+		strcpy(list[i], x);
+		i++;
+	}
 
 	list[i] = nullptr;  // We put the null to the last pointer to mark that the last element in list
 }
+
 void fwritenames_reverse(ofstream &f,char *list [])
 {
 	int i;
-	for(i = 0; list[i] != nullptr ; ++i)
-		;
+
+	for(i = 0; list[i] != nullptr; ++i);
 
 	for(int j = i-1; j >= 0 ; --j)
 	{
 		// your code is here
+		f << list[j] << endl;
 	}
 }
+
 void freenames(char *list [])
 {
 	for (int i = 0; list[i] != nullptr; ++i)
